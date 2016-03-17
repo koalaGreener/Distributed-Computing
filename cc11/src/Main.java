ce85a8646ef20c466d31c45f0ee32fa114e9dafa
HUANGWEIJIE <weijie.huang@ucl.ac.uk>
2016-03-17 22:53:51 +0000

    Finished the part B (1).

---
+++ cc11/src/Main.java
@@ -0,0 +1,85 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Mapper;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
+
+        private final static IntWritable one = new IntWritable(1);
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            String wordA;
+            String wordB;
+
+/*            String[] result = value.toString().split(" ");
+            for (int x = 0; x < result.length - 1; x++){
+                    Text word = new Text(result[x] + " " + result[x + 1]);
+                    context.write(word, one);
+            }*/
+
+            String line = value.toString();
+            StringTokenizer itr = new StringTokenizer(line);
+            //For each word, map it to a count of one. Duplicate words will be counted
+            //in the reduce phase.
+            if (itr.hasMoreTokens()){
+                wordA = itr.nextToken();
+                while (itr.hasMoreTokens()) {
+                    wordB = itr.nextToken();
+                    context.write(new Text(wordA + " " + wordB), one);
+                    wordA = wordB;
+            }
+
+            }
+
+
+
+        }
+    }
+
+
+    public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+
+            context.write(key, result);
+        }
+    }
+
+
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+
+        Configuration conf1 = new Configuration();
+        Job job = Job.getInstance(conf1, "bigram");
+
+        job.setJarByClass(Main.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        //job.setNumReduceTasks(1);
+        job.setNumReduceTasks(10);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        //outputPath.getFileSystem(conf1).delete(outputPath);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+
+
+    }
+
+}
